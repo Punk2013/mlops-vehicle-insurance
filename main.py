afc4b9fad43705c9fddb3ffb4579516d4f876832
Punk2013 <gdreference@gmail.com>
2025-05-09 22:34:51 +0300

    change requirements.txt

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import argparse
-import pickle
 import pathlib
+import pickle
+
 import pandas as pd
-from storage import Storage
-from analysis import Analyser
-from preprocess import Preprocessor
-from model import VehicleInsuranceModel
-from validation import Validator
-from maintenance import Maintainer
 from sklearn.svm import SVC
+
+from analysis import Analyser
+from maintenance import Maintainer
+from model import VehicleInsuranceModel
+from preprocess import Preprocessor
+from storage import Storage
+from validation import Validator
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="mlops")
-    parser.add_argument("-mode", choices=["inference", "update", "summary"],
-                        required=True)
+    parser.add_argument(
+        "-mode", choices=["inference", "update", "summary"], required=True
+    )
     parser.add_argument("-file", type=pathlib.Path)
 
     args = parser.parse_args()
@@ -21,14 +24,14 @@ if __name__ == "__main__":
     if args.mode == "inference":
         df = pd.read_csv(args.file)
         df = Analyser.basic_cleanup(df)
-        prep = Preprocessor('inference')       
+        prep = Preprocessor("inference")
         df = prep.preprocess_nans(df)
         X, _ = prep.preprocess(df)
         model = Maintainer.load_main_model()
         pred = model.predict(X)
         print(pred)
     elif args.mode == "update":
-        batchsize = 2000 # adjust how much data is added
+        batchsize = 2000  # adjust how much data is added
         storage = Storage(batchsize)
         batch = storage.get_batch()
         print(f"NEW BATCH:\n{batch}")
@@ -40,10 +43,10 @@ if __name__ == "__main__":
         df = Analyser.basic_cleanup(storage.train_data)
         print(f"DATASET AFTER BASIC CLEANUP:\n{df}")
 
-        prep = Preprocessor('train')
+        prep = Preprocessor("train")
         df = prep.preprocess_nans(df)
         print(f"DATASET AFTER NAN REMOVAL:\n{df}")
-        
+
         X, y = prep.preprocess(df)
         print(f"PREPROCESSED DATA\nX:\n{X}\ny:\n{y}")
 
@@ -54,7 +57,7 @@ if __name__ == "__main__":
         validator = Validator()
         scores = validator.test(model, X, y)
         print(scores)
-        
+
         Maintainer.save_main_model(model)
         storage.save_train_data()
     elif args.mode == "summary":
@@ -63,6 +66,6 @@ if __name__ == "__main__":
         for dict in stats:
             print(dict)
         print("CROSS VALIDATION SCORES:")
-        with open("cv.pkl", 'rb') as f:
+        with open("cv.pkl", "rb") as f:
             cv = pickle.load(f)
         print(cv)
